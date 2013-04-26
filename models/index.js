9fac3b0d7f4abe57d2a7ade3c73b20736b7781a3
Nils De Moor <nils.de.moor@gmail.com>
2013-04-26 17:03:43 +0200

    added config module

--- models/index.js
+++ models/index.js
@@ -1,14 +1,20 @@
 // As per http://redotheweb.com/2013/02/20/sequelize-the-javascript-orm-in-practice.html
 
 var Sequelize = require('sequelize');
-//var config    = require('config').database;  // we use node-config to handle environments
+var config    = require('config').Mysql;  // we use node-config to handle environments
 
 // initialize database connection
 var sequelize = new Sequelize(
-  '',//config.name,
-  '',//config.username,
-  ''//config.password,
-  //config.options
+  config.db,
+  config.user,
+  config.pass,
+  {
+    host: config.host,
+    define: { 
+      charset: 'utf8',
+      collate: 'utf8_general_ci' 
+    }
+  }
 );
 
 // load models
