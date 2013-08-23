6e82645d2e8b37e778fc6b56034809a6b62274e5
Loic lambrichts <loic@woorank.com>
2013-08-23 17:09:26 +0200

    js/css update

--- jsapp/js/main.js
+++ jsapp/js/main.js
@@ -9,105 +9,48 @@
 	// VARIABLES
 	var signInBoxTop 			= '-400px',
 		signInBoxAnimateSpeed 	= 600,
-		viewAnimateSpeed  		= 600;
+		viewAnimateSpeed  		= 600,
+		groupAlreadyLoaded 		= false,
+
+		isBoxShow				= false,
+
+		userAuth				= true;
+		//userAuth = sessionStorage.getItem('userAuth');
 
 
 	// METHODS
 
 
-	// Hide sign in box
-	var hideSignInBox = function() {
+	// ********************************************************************
+	// Hide the box
+	// ********************************************************************
+	var hideBox = function() {
 
-		$('.message').animate({'top' : signInBoxTop}, signInBoxAnimateSpeed);
+		$('.box').animate({'top' : signInBoxTop}, signInBoxAnimateSpeed);
 
 	};
 
 
 
+	// ********************************************************************
+	// Loading all groups and display and html table
+	// ********************************************************************
+	var loadAllGroups = function() {
 
-	// ONLOAD
-	$( function () {
-
-
-
-		// ********************************************************************
-		// Views actions like show and hide
-		// ********************************************************************
-
-		// Hide all view except the first view
-		$('.view').hide();
-
-		$('#home').show();
-
-		// Display a view when the user click on showView
-		$('body').on('click', '.showView', function() {
-
-			var view = $(this).attr('href');
-
-			$('.view').fadeOut(viewAnimateSpeed).hide();
-
-			$(view).fadeIn(viewAnimateSpeed);
-
-		});
-
-
-
-		// ********************************************************************
-		// All Sign In actions here
-		// ********************************************************************
-
-		// Show the sign in box
-		$('#signIn').on('click', function(e) {
-
-			e.preventDefault();
-
-			$('.message').animate({'top' : '100px'}, signInBoxAnimateSpeed);
-
-		});
-
-
-		// Hide sign in box
-		$('.btn-cancel').on('click', function() {
-			
-			hideSignInBox();
-
-		});
-
-		// Validate the sign in box
-		$('#formSignIn').on('submit', function() {
-
-			// Add user inscription here
-
-
-			alert('sign in yeah');
-
-			//$('#signIn').addClass('disabled');
-
-
-			hideSignInBox();
-
-			return false;
-
-		});
-
-
-		// ********************************************************************
-		// Join a group
-		// ********************************************************************
-
-		$('#btnJoin').on('click', function(){
+		if(!groupAlreadyLoaded) {
 
 			var groupsCounter 	= 0,
 				html 			= '';
 
-			$('body').hmapi('getUserById', 1, function(data){
-
+			$('body').hmapi('getAllGroup', function(data){
 
 				console.log(data);
 
 				groupsCounter = data.length;
 
 				if(groupsCounter !== 0) {
+
+					groupAlreadyLoaded = true;
 
 					$('#groups tbody').first('tr').hide();
 
@@ -123,6 +66,141 @@
 
             });
 
+		}
+
+	};
+
+
+	var startViews = function() {
+
+		$('.view').hide();
+
+		$('#home').show();
+
+	};
+
+
+
+	// ONLOAD
+	$( function () {
+
+
+		// ********************************************************************
+		// Views actions (show and hide)
+		// ********************************************************************
+
+		// Hide all view except the first view
+		startViews();
+
+		// Display a view when the user click on showView
+		$('body').on('click', '.showView', function() {
+
+			var view = $(this).attr('href');
+
+			$('.view').fadeOut(viewAnimateSpeed).hide();
+
+			$(view).fadeIn(viewAnimateSpeed);
+
+		});
+
+		// If the user is logged, we show the group page
+		if(userAuth) {
+			
+			console.log('loggé');
+
+			$('#home').hide();
+
+			$('#groups').show();
+
+			new loadAllGroups();
+
+
+			$('<a href="#" id="loggOut">Logout</a>').insertAfter('header');
+		}
+
+
+
+
+		// ********************************************************************
+		// All loggin/loggout actions here
+		// ********************************************************************
+		
+		// Hide box on cancel button click
+		$('.btn-cancel').on('click', function() {
+			
+			hideBox();
+
+			isBoxShow = false;
+
+		});
+
+		// Show the sign in box
+		$('#signIn').on('click', function(e) {
+
+			e.preventDefault();
+
+			if(isBoxShow) hideBox();
+
+			$('.boxSignIn').animate({'top' : '100px'}, signInBoxAnimateSpeed);
+
+			isBoxShow = true;
+
+		});
+
+		// Validate the sign in box
+		$('#formSignIn').on('submit', function() {
+
+			// Add user inscription here
+
+
+			alert('sign in yeah');
+
+			//$('#signIn').addClass('disabled');
+
+
+			hideBox();
+
+			return false;
+
+		});
+
+
+		// Show the loggin box
+		$('#loggIn').on('click', function(e) {
+
+			e.preventDefault();
+
+			if(isBoxShow) hideBox();
+
+			$('.boxLoggIn').animate({'top' : '100px'}, signInBoxAnimateSpeed);
+
+			isBoxShow = true;
+
+		});
+
+
+		// Logout user
+		$('body').on('click', '#loggOut', function(e) {
+
+			$('#loggOut').remove();
+
+			startViews();
+
+
+			console.log('loggout');
+
+			userAuth = false;
+
+		});
+
+
+		// ********************************************************************
+		// Liste group page
+		// ********************************************************************
+
+		$('#btnJoin').on('click', function(){
+
+			new loadAllGroups();
 
 		} );
 	
