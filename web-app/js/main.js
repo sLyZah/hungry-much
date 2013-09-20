bf26e5ca836bcbb1e7b2df4ed312709e9db6d3d9
Seb <seb@woorank.com>
2013-09-20 18:04:30 +0200

    i was going nuts

--- web-app/js/main.js
+++ web-app/js/main.js
@@ -65,6 +65,7 @@ angular.module('hungryMuch', ['ngRoute']).config(function ($routeProvider) {
 });
 
 angular.module('hungryMuch').constant('config', {
-  baseUrl: 'http://localhost:3000'
+  //baseUrl: 'http://localhost:3000'
   //baseUrl: 'http://ec2-46-137-47-154.eu-west-1.compute.amazonaws.com:3000'
+  baseUrl: 'http://10.10.0.22:3000'
 });
