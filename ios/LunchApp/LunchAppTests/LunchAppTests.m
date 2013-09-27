44eacb15d2362cd8a1444554e15ad4a69438cadb
Olivier Kaisin <olivier.kaisin@gmail.com>
2013-09-27 10:58:04 +0200

    Initial Commit

---
+++ ios/LunchApp/LunchAppTests/LunchAppTests.m
@@ -0,0 +1,34 @@
+//
+//  LunchAppTests.m
+//  LunchAppTests
+//
+//  Created by Olivier Kaisin on 27/09/13.
+//  Copyright (c) 2013 WooRank. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LunchAppTests : XCTestCase
+
+@end
+
+@implementation LunchAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
