1d4e37b4bca32e42bd74ac81e39578ef1404e8ca
Sylvain <marien.sylvain@gmail.com>
2013-10-04 17:35:27 +0200

    Icone + App_name + Theme modification

--- android/gen/com/example/hungrymuch/BuildConfig.java
+++ android/gen/com/example/hungrymuch/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.myfirstapp;
+package com.example.hungrymuch;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- android/gen/com/example/hungrymuch/R.java
+++ android/gen/com/example/hungrymuch/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.myfirstapp;
+package com.example.hungrymuch;
 
 public final class R {
     public static final class attr {
